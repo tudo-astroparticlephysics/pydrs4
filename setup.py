1772858f72a5e08a9114043fc8a851784ce99eeb
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-01-27 12:54:45 +0100

    Fix setup

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
-    name='drs',
+    name='drs4',
     author='Maximilian Nöthe',
     author_email='maximilian.noethe@tu-dortmund.de',
+    packages=find_packages(),
     version='0.0.1',
     install_requires=['numpy'],
     tests_require=['pytest'],
