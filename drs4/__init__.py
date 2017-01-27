494f2b97ea8c57ec61ab67476be3d7d14a8d7dda
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-01-27 12:55:23 +0100

    Add lowpass filter

--- drs4/__init__.py
+++ drs4/__init__.py
@@ -1,3 +1,4 @@
 from .binio import DRS4BinaryFile
+from .signal_processing import butter_lowpass_filter
 
 __all__ = ['DRS4BinaryFile', ]
---
+++ drs4/signal_processing.py
@@ -0,0 +1,20 @@
+from scipy.signal import butter, filtfilt
+
+
+def butter_lowpass(cutoff_, sample_freq, order=5):
+    '''
+    See http://stackoverflow.com/a/25192640/3838691
+    '''
+    nyq = 0.5 * sample_freq
+    normal_cutoff = cutoff_freq / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+def butter_lowpass_filter(data, cutoff_freq, sample_freq, order=5):
+    '''
+    See http://stackoverflow.com/a/25192640/3838691
+    and http://stackoverflow.com/a/13740532/3838691
+    '''
+    b, a = butter_lowpass(cutoff_freq, sample_freq, order=order)
+    y = filtfilt(b, a, data)
+    return y
